84fcdbb3c8b3a86358826aae1afe6262f3ebee53
Kouromo <blackwolf4728@gmail.com>
2025-12-13 18:05:43 +0100

    Layout fleche

--- Assets/Scripts/Grid/GridCell.cs
+++ Assets/Scripts/Grid/GridCell.cs
@@ -1,26 +1,62 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI; // IMPORTANT : Ajoutez l'import pour les composants UI
 
 public class GridCell : MonoBehaviour
 {
-    [SerializeField] private TextMeshPro letterText;
+    [SerializeField]
+    private TMP_InputField inputField;
 
+    [SerializeField]
+    private Image background;
+
+
+    public Grid.GridManager manager;
+
+    // Données de position et de lettre
     public int X { get; private set; }
     public int Y { get; private set; }
+    public char CorrectLetter { get; private set; }
 
-    public void Initialize(int x, int y, char letter, float cellSize)
+    public void Initialize(int x, int y, char correctLetter, Grid.GridManager gridManager)
     {
         X = x;
         Y = y;
+        CorrectLetter = correctLetter;
+        manager = gridManager;
 
-        transform.position = new Vector3(x * cellSize, y * cellSize, 0f);
 
-        letterText.text = (letter != '\0') ? letter.ToString() : "";
+        //inputField.onValueChanged.AddListener(OnLetterEntered);
     }
 
+    // Méthode appelée lors de la saisie
+    //private void OnLetterEntered(string letter)
+    //{
+    //    if (string.IsNullOrEmpty(letter)) return;
+
+    //    //char enteredChar = letter.ToUpper()[0];
+
+    //    // Appel du Manager pour vérifier la lettre
+    //    // manager.CheckLetter(this, enteredChar);
+    //}
 
     public void Highlight(bool isHighlighted)
     {
-        GetComponent<SpriteRenderer>().color = isHighlighted ? Color.yellow : Color.white;
+        // Utiliser l'Image (background) au lieu du SpriteRenderer
+        background.color = isHighlighted ? Color.yellow : Color.white;
+
+        // Mettre le focus sur le champ de saisie
+        if (isHighlighted)
+            inputField.Select();
     }
+
+    //public void SetBackground(Color color)
+    //{
+    //    background.color = color;
+    //}
+
+    //public void SetInputInteractable(bool interactable)
+    //{
+    //    inputField.interactable = interactable;
+    //}
 }
--- Assets/Scripts/Grid/GridManager.cs
+++ Assets/Scripts/Grid/GridManager.cs
@@ -8,15 +8,18 @@ namespace Grid
         public Word[] wordsLevelOne;
         //private Word[] wordsLevelTwo;
         private char[,] _solutionGrid;
-        private char[,] _playerGrid;
 
         private bool _isLevelCompleted;
+
+        private GridCell[,] _cellObjects;
+
+        public RectTransform gridContainer;
 
         [Tooltip("Prefab for grid cell")]
         public GameObject cellPrefab;
 
-        [Tooltip("Parent GameObject of all cells")]
-        public Transform gridParent;
+        [Tooltip("Prefab for empty grid cell")]
+        public GameObject emptyCellPrefab;
 
 
 
@@ -24,13 +27,14 @@ namespace Grid
         void Start()
         {
             InitializeGrids(wordsLevelOne);
-            //DisplayGrid();
+
+            _cellObjects = new GridCell[GRID_SIZE, GRID_SIZE];
+            DisplayGrid();
         }
 
         void InitializeGrids(Word[] wordsLevel)
         {
             _solutionGrid = new char[GRID_SIZE, GRID_SIZE];
-            _playerGrid = new char[GRID_SIZE, GRID_SIZE];
 
             foreach (var data in wordsLevel)
                 PlaceWordOnGrid(data, _solutionGrid);
@@ -49,7 +53,7 @@ namespace Grid
                     x++;
                 }
 
-            else // Vertical
+            else
                 for (int i = 0; i < data.word.Length; i++)
                 {
                     grid[x, y] = data.word[i];
@@ -59,9 +63,56 @@ namespace Grid
 
         private void DisplayGrid()
         {
+            for (ushort y = 0; y < GRID_SIZE; y++)
+            {
+                for (ushort x = 0; x < GRID_SIZE; x++)
+                {
+                    if (_solutionGrid[x, y] != '\0')
+                    {
+                        GameObject cellObj = Instantiate(cellPrefab, gridContainer);
+                        GridCell cellScript = cellObj.GetComponent<GridCell>();
 
+                        // Récupération de la lettre de solution
+                        char correctLetter = _solutionGrid[x, y];
+
+                        // Initialisation : on passe le Manager lui-même
+                        cellScript.Initialize(x, y, correctLetter, this);
+
+                        // Stocker la référence
+                        _cellObjects[x, y] = cellScript;
+                    }
+                    else
+                    {
+                        Instantiate(emptyCellPrefab, gridContainer);
+                    }
+                }
+            }
         }
 
-        
+        // Nouvelle méthode de vérification appelée par GridCell.cs
+        /*public void CheckLetter(GridCell cell, char enteredChar)
+        {
+            // 1. Mise à jour de la grille du joueur
+            _playerGrid[cell.X, cell.Y] = enteredChar;
+
+            // 2. Vérification immédiate
+            if (enteredChar == cell.CorrectLetter)
+            {
+                Debug.Log($"Correct ! ({cell.X},{cell.Y})");
+                cell.SetBackground(Color.green);
+                cell.SetInputInteractable(false);
+
+                // 3. (À ajouter) Vérifier si le mot est complet et si le niveau est terminé
+                // CheckWordCompletion(cell.X, cell.Y);
+                // CheckLevelCompletion();
+            }
+            else
+            {
+                Debug.Log($"Faux. Attendu : {cell.CorrectLetter}");
+                cell.SetBackground(Color.red); //
+            }
+        }*/
+
+
     }
 }
